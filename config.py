faeff5f7c20433254c09c9e312bc0def52f0f092
Dhinoshni K <dhinoshnikandasamy@gmail.com>
2026-09-06 19:43:31 +0530

    Prepare backend for Render

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
 import os
 
-class Config:
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "root"   #  MySQL password
-    MYSQL_DB = "bank_db"
 
-    JWT_SECRET_KEY = "bank_management_secret_key_2026"
-    JWT_EXPIRATION_HOURS = 1
-    JWT_ALGORITHM = "HS256"
+class Config:
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
+    MYSQL_DB = os.getenv("MYSQL_DB", "bank_db")
+
+    JWT_SECRET_KEY = os.getenv(
+        "JWT_SECRET_KEY",
+        "bank_management_secret_key_2026"
+    )
+
+    JWT_EXPIRATION_HOURS = int(
+        os.getenv("JWT_EXPIRATION_HOURS", "1")
+    )
+
+    JWT_ALGORITHM = os.getenv(
+        "JWT_ALGORITHM",
+        "HS256"
+    )
